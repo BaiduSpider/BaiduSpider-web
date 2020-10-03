665582799a9baa96874b531a23510870c1dec523
samzhangjy <samzhang951@outlook.com>
2020-10-03 16:55:28 +0800

    :tada: 创建BaiduSpider网页版

---
+++ src/config/config.js
@@ -0,0 +1,3 @@
+export default {
+  serverURL: process.env.serverURL || 'https://baiduspider-5f17b.asia1.kintocloud.com/'
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <SearchBox/>
+</template>
+
+<script>
+import SearchBox from '@/components/SearchBox'
+
+export default {
+  name: 'Home',
+  components: {
+    SearchBox
+  },
+  created: function () {
+    document.title = 'BaiduSpider'
+  }
+}
+</script>
