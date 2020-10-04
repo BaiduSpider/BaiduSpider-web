d4c37fd575aed8e5008c7480504266ca4be6ef52
samzhangjy <samzhang951@outlook.com>
2020-10-04 18:09:43 +0800

    :ambulance: 更改API地址

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export default {
-  serverURL: process.env.serverURL || 'https://baiduspider-5f17b.asia1.kintocloud.com/'
+  serverURL: process.env.serverURL || 'https://baiduspider-api-a5a359.asia1.kinto.io'
 }
